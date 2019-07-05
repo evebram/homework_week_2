1cfe83a3066ce05203134f874b93669a2b494a3e
= <evebramley@me.com>
2019-07-05 16:40:42 +0100

    adding new room

--- rooms.rb
+++ rooms.rb
@@ -2,10 +2,39 @@ class Rooms
 
   attr_reader :name, :capacity
 
-  def initialize(name, capacity)
+  def initialize(name, capacity, occupants)
     @name = name
     @capacity = capacity
+    @occupants = occupants
     @playlist = []
   end
+
+  def count_guests()
+    return @occupants.size
+  end
+
+  def check_into_room(guest)
+    @occupants << guest
+  end
+
+  def check_out_of_room(guest)
+    return @occupants.delete(guest)
+  end
+
+  def check_playlist()
+    return @playlist.size
+  end
+
+  def add_song_to_playlist(song)
+    @playlist << song
+  end
+
+  def check_for_space_in_room(capacity)
+      if @occupants <= capacity
+        return true
+      else
+        return false
+      end
+    end
 
 end
--- specs/rooms_spec.rb
+++ specs/rooms_spec.rb
@@ -1,11 +1,25 @@
 require('minitest/autorun')
 require('minitest/rg')
 require_relative('../rooms.rb')
+require_relative('../guests.rb')
+require_relative('../songs.rb')
 
 class RoomsTest < MiniTest::Test
 
   def setup
-    @room = Rooms.new("Room 1", 8)
+
+    @song1 = Songs.new("Stay With Me")
+    @song2 = Songs.new("Toxic")
+    @song3 = Songs.new("Lose Yourself")
+
+    @guest1 = Guests.new("Jessica", 9, @song1)
+    @guest2 = Guests.new("Susan", 3, @song2)
+    @guest3 = Guests.new("Frida", 5, @song1)
+
+    @occupants = [@guest1, @guest2, @guest3]
+
+    @room = Rooms.new("Room 1", 4, @occupants)
+
   end
 
   def test_room_class()
@@ -17,7 +31,36 @@ class RoomsTest < MiniTest::Test
   end
 
   def test_room_capacity()
-    assert_equal(8, @room.capacity )
+    assert_equal(4, @room.capacity)
   end
+
+  def test_room_occupants()
+    assert_equal(3, @room.count_guests())
+  end
+
+  def test_check_into_room()
+    @guest4 = Guests.new("Kenny", 2, @song2)
+    @room.check_into_room(@guest4)
+    assert_equal(4, @room.count_guests())
+  end
+
+  def test_check_out_of_room()
+    @room.check_out_of_room(@guest1)
+    assert_equal(2, @room.count_guests())
+  end
+
+  def test_add_song_to_playlist()
+    @room.add_song_to_playlist(@song3)
+    assert_equal(1, @room.check_playlist())
+  end
+
+  def test_can_guest_enter_room__true()
+    @room.check_for_space_in_room(@guest1, @room.capacity)
+    assert_equal(true, )
+  end
+
+  # def test_thats_my_jam()
+  #
+  # end
 
 end
